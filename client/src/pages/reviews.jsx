850f5b4800f684b27a6af0e4dcd099ce18a06e87
Lital Levin <lital.levin@gmail.com>
2020-04-23 17:53:57 +0200

    remove title from review banner

--- client/src/pages/reviews.jsx
+++ client/src/pages/reviews.jsx
@@ -5,7 +5,6 @@ import ArticleList from '../components/articles/article_list';
 const Reviews = () => (
     <div className="content">
       <div className="banner" style={{backgroundImage: `url('https://res.cloudinary.com/litall/image/upload/v1587589378/dovalfon/reviews-banner-3_dcigih.png')` }}>
-        <h2>Praise for the book</h2>
       </div>
       <ArticleList articleTag="Review"/>
     </div>
