7c930104f07ddc061c6100be0689b3e44f69e95d
Lital Levin <lital.levin@gmail.com>
2020-04-23 17:41:40 +0200

    add interviews to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -441,7 +441,65 @@ pub_date: "2019-01-29",
 credit: "Paul Burke",
 excerpt: "A Long Night in Paris is one of the most enjoyable action spy thrillers I’ve read in a long time. Exciting and intriguing, a real page turner that grips instantly and stays true and fast to the end, a novel that has cut to the chase from page one. But if it was just action it wouldn’t hold my interest for 400 pages and it does, this is also a novel that has real depth.",
 importance: 3
-}
+},
+{title: "Surveillance : pour Dov Alfon, « si vous êtes suivi·e, c'est par des personnes situées à 4000 km de vous »",
+   source: "France Inter",
+  tag: "Interview",
+  language: "French",
+    media: "video",
+  video_url: "https://www.franceinter.fr/embed/player/aod/fb4abdb7-0f44-47b3-af80-37b5c2a78e9e",
+
+},
+
+  {title: "Un pays, un auteur: en Israël avec Dov Alfon",
+   source: "Radio France Internationale",
+  tag: "Interview",
+  language: "English",
+    media: "audio",
+  url: "http://www.rfi.fr/fr/emission/20190712-alfon-dov-israel-roman-unite-8200-redacteur-haaretz"
+},
+{title: "Interview de Dov Alfon : «Je voulais que le lecteur ait l'impression d'être pourchassé... »",
+   source: "fnac",
+   tag: "Interview",
+   language: "French",
+     media: "video",
+  video_url: "https://www.youtube.com/embed/QMAYQuQsg-g"
+},
+{title: "L'INVITÉ DE 8H20: LE GRAND ENTRETIEN",
+   source: "France Inter",
+   tag: "Interview",
+   language: "French",
+     media: "video",
+  video_url: "https://www.dailymotion.com/embed/video/x75bp5l"
+},
+{title: "Dov Alfon interview for Série Series",
+   source: "Série Series",
+   tag: "Interview",
+     media: "video",
+   language: "French",
+  video_url: "https://player.vimeo.com/video/391782004"
+},
+{title: "L'invitée de Bourdin Direct: Dov Alfon",
+   source: "RMC",
+   language: "French",
+   media: "audio",
+   tag: "Interview",
+  url: "https://rmc.bfmtv.com/mediaplayer/audio/rmc-0904-l-invitee-de-bourdin-direct-dovalfon-464468.html"
+},
+{title: "Retour sur nos enquêtes. Netanyahou pris dans l'affaire du CO2",
+   source: "Mediapart",
+   language: "French",
+   tag: "Interview",
+     media: "video",
+  video_url: "https://www.dailymotion.com/embed/video/x4fk1vt"
+},
+{title: "Dov Alfon vous présente son ouvrage Unité 8200",
+  source: "librairie mollat",
+  language: "French",
+  tag: "Interview",
+  media: "video",
+  video_url: "https://www.youtube.com/embed/VzgeOZ_zdVU"
+},
 ])
 
 
