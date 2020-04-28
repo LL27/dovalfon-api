cda5edde912b47d12a032a9131bbded7eb3d7dee
Lital Levin <lital.levin@gmail.com>
2020-04-28 15:41:12 +0200

    contact page

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,8 +1,33 @@
 import React from 'react'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter, faInstagram, faFacebook } from '@fortawesome/free-brands-svg-icons'
+
+
 const Contact = () => (
-  <div className="page">
-    <h2>Contact</h2>
+  <div className="contact-page-wrapper container">
+    <div className="contact-page">
+      <div className="contact-left box-card">
+        <h1>GET <br/>IN <br/>TOUCH</h1>
+      </div>
+
+
+      <div className="contact-right box-card">
+        <div className="social">
+
+          On Twitter: <a href="https://twitter.com/dovalfon"><FontAwesomeIcon icon={faTwitter} /></a> //
+          On Instagram: <a href="https://www.instagram.com/dovalfon/?hl=fr"><FontAwesomeIcon icon={faInstagram} /></a> //
+          On Facebook:  <a href="https://www.facebook.com/dovalfon"><FontAwesomeIcon icon={faFacebook} /></a>
+        </div>
+
+        <div className="curtis">
+           <p>Stephanie Thwaites, Curtis Brown Books</p>
+        <p>+44 (0)20 7393 4411</p>
+        <p><a href="isobel.gahan@CURTISBROWN.CO.UK" data-event-type="email">isobel.gahan@CURTISBROWN.CO.UK</a></p>
+        </div>
+
+    </div>
+   </div>
   </div>
 );
 export default Contact;
